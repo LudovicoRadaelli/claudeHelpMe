9a7a469c2f070f1c8008715eb0c9174a24b6374f
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2026-08-04 13:05:09 +0200

    Add files via upload

---
+++ esercizi/argomenti.js
@@ -0,0 +1,8 @@
+window.ARGOMENTI = [
+  { cartella: "1fondamenti",     titolo: "Fondamenti" },
+  { cartella: "2equazioneRetta", titolo: "Equazione della retta" },
+  { cartella: "3funzioni",       titolo: "Funzioni" },
+  { cartella: "4boh",       titolo: "Derivata"},
+  { cartella: "6boh",       titolo: "Integrali"},
+  { cartella: "7boh",       titolo: "Equazioni differenziali"}
+];
